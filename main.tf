18875430d570c8d04d097dcb3b85f661474cc3b6
wcywong69 <wcywong69@gmail.com>
2025-07-10 20:34:19 +0800

    Adding new s3 bucket

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sctp-ce10-tfstate"
+    key    = "3-2-activity.tfstate"  #Change this
+    region = "ap-southeast-1"
+  }
+}
+
+resource "aws_s3_bucket" "s3_tf" {
+  bucket_prefix =   "wong-3-2-activity-bucket" # Set your bucket name here
+}

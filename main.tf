30c2a8f6dc7262c4a5f30f3a4f16f0be4856d38c
wcywong69 <wcywong69@gmail.com>
2025-07-10 21:29:25 +0800

    Correction

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "sctp-ce10-tfstate"
-    key    = "3-2-activity.tfstate"  #Change this
+    key    = "3-2-activity.tfstate" #Change this
     region = "ap-southeast-1"
   }
 }
 
 resource "aws_s3_bucket" "s3_tf" {
-  bucket_prefix =   "wong-3-2-activity-bucket" # Set your bucket name here
+  bucket_prefix = "wong-3-2-activity-bucket" # Set your bucket name here
 }
